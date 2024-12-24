a6f0ecefaf1a4c1dbd7a54e639650ac8dd632012
rohitroy@github <rhtry.tech@gmail.com>
2024-12-24 16:46:47 +0530

    add: initial setup

---
+++ backend-springboot/src/test/java/springdev/backend_springboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package springdev.backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='font-montserrat text-sm'>
+      <h3>Rohit</h3>
+     </div>
+    </>
+  )
+}
+
+export default App
